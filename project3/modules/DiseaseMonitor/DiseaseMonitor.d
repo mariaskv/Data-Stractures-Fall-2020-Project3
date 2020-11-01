bd2493165128210d2c3943d541b73fe346101aef
Maria Skevofylaka <maria.skev2001@gmail.com>
2020-11-01 13:33:56 +0200

    Delete DiseaseMonitor.d

--- project3/modules/DiseaseMonitor/DiseaseMonitor.d
+++
@@ -1,4 +0,0 @@
-../modules/DiseaseMonitor/DiseaseMonitor.o: \
- ../modules/DiseaseMonitor/DiseaseMonitor.c ../include/DiseaseMonitor.h \
- ../include/ADTList.h ../include/common_types.h ../include/ADTMap.h \
- ../include/ADTPriorityQueue.h ../include/ADTVector.h ../include/ADTSet.h
